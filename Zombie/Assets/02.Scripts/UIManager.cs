f76569a7c8f61f226170a8e43fdbc7098412d62a
kmy7102 <jim5907@naver.com>
2022-05-24 16:33:17 +0900

    0524

---
+++ Zombie/Assets/02.Scripts/UIManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    public void UpdateAmoText(int magAmmo, int remainAmmo){
+
+    }
+}
